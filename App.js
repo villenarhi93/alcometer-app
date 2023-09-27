695ab38e686e0cc592f3973ce33e79cfd73a4de2
Ville Närhi <villenarhi@hotmail.fi>
2023-09-27 17:31:31 +0300

    adding to github

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Switch, ScrollView, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { LightMode, DarkMode } from './styles/Styles';
+import { useState } from 'react';
+import { RadioButton } from 'react-native-paper';
+import NumericInput from 'react-native-numeric-input';
 
 export default function App() {
+
+  const [isOn, setIsOn] = useState(false);
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [sex, setSex] = useState('male');
+  const [result, setResult] = useState(0);
+
+  const toggleSwitch = () => setIsOn(isOn => !isOn);
+
+  function calculate() {
+
+    let litres = Number(bottles) * 0.33
+    let grams = litres * 8 * 4.5
+    let burning = Number(weight) / 10
+    let gramsLeft = grams - burning * Number(hours)
+
+    if ( sex == 'male') {
+      let result = gramsLeft / (Number(weight) * 0.7)
+      setResult(result) } else {
+      let result = gramsLeft / (Number(weight) * 0.6)
+      setResult(result)
+      }
+    
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={LightMode.containter}>
+      <ScrollView>
+        <View style={LightMode.switchRow}>
+          <Switch
+            style={{ transform: [{ scaleX: 1.4 }, { scaleY: 1.4 }] }}            
+            value={isOn}
+            onValueChange={toggleSwitch}
+            thumbColor='#18e780'
+            trackColor={{ false:'#e25412', true:'#ffffff'}}
+    
+          />
+        </View>
+        <Text style={LightMode.header}>Alcometer</Text>
+        <Text style={LightMode.text}>Weight</Text>
+          <TextInput
+            style={LightMode.input} 
+            onChangeText={setWeight} 
+            value={weight}
+            keyboardType="numeric"
+          />
+        <Text style={LightMode.text}>Bottles</Text>
+            <View style={LightMode.numeric}>
+                <NumericInput
+                  onChange={b => setBottles(b)}
+                  value={bottles}
+                  totalHeight={55}
+                  totalWidth={175}
+                  rounded
+                  minValue={0}
+                  borderColor='#ffffff'
+                  rightButtonBackgroundColor='#18e780'
+                  leftButtonBackgroundColor='#18e780'
+                  textColor='#e25412'
+                  iconStyle={{color:'#ffffff'}}
+                />
+            </View>
+        <Text style={LightMode.text}>Hours</Text>
+            <View style={LightMode.numeric}>
+                <NumericInput
+                  onChange={h => setHours(h)}
+                  value={hours}
+                  totalHeight={55}
+                  totalWidth={175}
+                  rounded
+                  minValue={0}
+                  borderColor='#ffffff'
+                  rightButtonBackgroundColor='#18e780'
+                  leftButtonBackgroundColor='#18e780'
+                  textColor='#e25412'
+                  iconStyle={{ color: '#ffffff' }}
+                />
+            </View>
+        <RadioButton.Group onValueChange={newValue => setSex(newValue)} value={sex}>
+          <View style={LightMode.radio}>
+            <RadioButton value='male' color='#18e780'/>
+            <Text style={LightMode.text}>Male</Text>
+          </View>
+          <View style={LightMode.radio}>
+            <RadioButton value='female' color='#18e780' />
+            <Text style={LightMode.text}>Female</Text>
+          </View>
+        </RadioButton.Group>
+        <Text style={LightMode.result}>{result.toFixed(2)}</Text>
+        <TouchableOpacity style={LightMode.button}>
+          <Text style={LightMode.buttonText} onPress={calculate}>Calculate</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/Styles.js
@@ -0,0 +1,88 @@
+import { StatusBar, StyleSheet } from "react-native";
+
+
+const LightMode = StyleSheet.create({
+    containter: {
+        marginTop: StatusBar.currentHeight + 5,
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'stretch',
+        justifyContent: 'space-around'
+    },
+    switchRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: "flex-start",
+        marginLeft: 20
+    },
+    switch: {
+        alignSelf: "flex-start",
+    },
+    header: {
+        fontWeight: 'bold',
+        fontSize: 46,
+        textAlign: 'center',
+        color: '#18e780'
+    },
+    text: {
+        fontWeight: 'bold',
+        padding: 10,
+        fontSize: 28,
+        marginBottom: 10,
+        color: '#e25412'
+    },
+    input: {
+        height: 40,
+        borderWidth: 3,
+        marginLeft: 15,
+        marginRight: 15,
+        borderRadius: 10,
+        fontSize: 20,
+        paddingLeft: 10,
+        fontWeight: 'bold',
+        borderColor: '#18e780'
+    },
+    result: {
+        fontSize: 38,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 20,
+        color: '#18e780'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 10,
+        borderWidth: 2,
+        borderRadius: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        backgroundColor: '#e25412',
+        borderColor: '#ffffff'
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 28,
+        color:'#ffffff'
+    },
+    radio: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginTop: 15,
+        color:'#18e780'
+    },
+    numeric: {
+        marginLeft: 15
+    }
+});
+
+
+const DarkMode = StyleSheet.create({
+    containter: {
+        ...LightMode.containter,
+    }
+});
+
+
+export {LightMode, DarkMode};
